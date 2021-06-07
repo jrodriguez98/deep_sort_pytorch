bf09c24437c7248c37e143ffc13d6ec469d7c75e
Jose Rodriguez <jrodriguez98@correo.ugr.es>
2021-06-07 10:06:11 +0200

    Adaptar test.py a nuestro proyecto

--- deep_sort/deep/test.py
+++ deep_sort/deep/test.py
@@ -5,12 +5,19 @@ import torchvision
 import argparse
 import os
 from loader import CustomFolder
-from model import Net
+from model_sq import Net
+
+def get_model_name(model_path):
+    model_name = model_path.split(sep="/")[-1]
+    model_name = model_name.split(sep=".")[0]
+
+    return model_name
 
 parser = argparse.ArgumentParser(description="Train on market1501")
-parser.add_argument("--data-dir",default='data',type=str)
-parser.add_argument("--no-cuda",action="store_true")
-parser.add_argument("--gpu-id",default=0,type=int)
+parser.add_argument("--data-dir", default='data', type=str)
+parser.add_argument("--model", default='checkpoint/ckpt.t7', type=str)
+parser.add_argument("--no-cuda", action="store_true")
+parser.add_argument("--gpu-id", default=0, type=int)
 args = parser.parse_args()
 
 # device
@@ -20,10 +27,11 @@ if torch.cuda.is_available() and not args.no_cuda:
 
 # data loader
 root = args.data_dir
+model_path = args.model
 query_dir = os.path.join(root, "query")
 gallery_dir = os.path.join(root, "gallery")
 transform = torchvision.transforms.Compose([
-    torchvision.transforms.Resize((128,64)),
+    torchvision.transforms.Resize((128,128)),
     torchvision.transforms.ToTensor(),
     torchvision.transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
 ])
@@ -37,10 +45,10 @@ galleryloader = torch.utils.data.DataLoader(
 )
 
 # net definition
-net = Net(num_classes=752, reid=True)
-assert os.path.isfile("./checkpoint/ckpt.t7"), "Error: no checkpoint file found!"
-print('Loading from checkpoint/ckpt.t7')
-checkpoint = torch.load("./checkpoint/ckpt.t7")
+net = Net(num_classes=34, reid=True)
+assert os.path.isfile(model_path), "Error: no checkpoint file found!"
+print(f'Loading from {model_path}')
+checkpoint = torch.load(model_path)
 net_dict = checkpoint['net_dict']
 net.load_state_dict(net_dict, strict=False)
 net.eval()
@@ -57,14 +65,14 @@ with torch.no_grad():
         inputs = inputs.to(device)
         features = net(inputs).cpu()
         query_features = torch.cat((query_features, features), dim=0)
-        print(labels)
+        #print(labels)
         query_labels = torch.cat((query_labels, labels))
 
     for idx,(inputs,labels) in enumerate(galleryloader):
         inputs = inputs.to(device)
         features = net(inputs).cpu()
         gallery_features = torch.cat((gallery_features, features), dim=0)
-        print(labels)
+        #print(labels)
         gallery_labels = torch.cat((gallery_labels, labels))
 
 #gallery_labels -= 2
@@ -77,5 +85,10 @@ features = {
     "gl": gallery_labels
 }
 
+if not os.path.exists("features"):
+    os.makedirs("features")
+
+model_name = get_model_name(model_path)
+
 print("Guardando features")
-torch.save(features, "features.pth")
+torch.save(features, f"features/{model_name}.pth")
